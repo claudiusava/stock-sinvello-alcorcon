91597e1aed3d04d6f53f1ef43b38befda2795f4e
Claudiu <4c.claudiusava@gmail.com>
2026-08-03 13:52:14 +0200

    feat: prueba con ipad

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+alert('Inicio de main.ts');
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+alert('Antes de bootstrap');
+
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .then(() => {
+    alert('Bootstrap completado');
+  })
+  .catch((err) => {
+    alert(`Error: ${err}`);
+    console.error(err);
+  });
