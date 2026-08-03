0f03903de24288ab3056dd1cc13e50f4f895831e
Claudiu <4c.claudiusava@gmail.com>
2026-08-03 14:04:26 +0200

    fix: marcha atrás a prueba de ipad

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
-alert('Inicio de main.ts');
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-alert('Antes de bootstrap');
-
 bootstrapApplication(AppComponent, appConfig)
-  .then(() => {
-    alert('Bootstrap completado');
-  })
-  .catch((err) => {
-    alert(`Error: ${err}`);
-    console.error(err);
-  });
+  .catch((err) => console.error(err));
